63bb78f13dddc66d204196ef2ad9d03fbd091c0e
Andrey Valenko <andreyvalenko@gmail.com>
2023-02-06 22:40:59 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,6 @@
 export default function fetchCountries(name) {
   return fetch(`https://restcountries.com/v2/name/${name}`).then(response => {
+    console.log(response);
     return response.json();
   });
 }
