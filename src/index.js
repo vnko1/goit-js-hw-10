21a5e3debc2ed95847ce70bf902f31783566d6fc
Andrey Valenko <andreyvalenko@gmail.com>
2023-02-06 17:10:08 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 import fetchCountries from './js/fetchCountries';
-console.log(fetchCountries);
+
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.getElementById('search-box');
+const listEl = document.querySelector('.country-list');
+const infoContainer = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(getValue, DEBOUNCE_DELAY));
+
+function getValue(e) {
+  const inputValue = e.target.value.trim();
+  if (inputValue !== '') {
+    fetchCountries(inputValue);
+  }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,7 @@
-export default function fetchCountries(name) {}
+export default function fetchCountries(name) {
+  fetch(`https://restcountries.com/v2/name/${name}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => console.log(data));
+}
