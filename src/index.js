9f9eb45d99b44d0d333be4fddbca19f8c5e22eb6
Andrey Valenko <andreyvalenko@gmail.com>
2023-02-07 13:02:54 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import fetchCountries from './js/fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v2/name/';
 const searchParams = '?fields=name,capital,population,flags,languages';
 
-export default function fetchCountries(name) {
+export function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}${searchParams}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
